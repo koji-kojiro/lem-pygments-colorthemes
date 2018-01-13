928f03ee9f2a516f79b17c022f082eee53b3c74b
TANI Kojiro <kojiro0531@gmail.com>
2018-01-13 19:55:17 +0900

    Fix to convert colors which is not defined in lem

--- build.py
+++ build.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import pygments.styles
+from x256 import x256
 from pygments.token import Text
 
 translation_map = {
@@ -20,15 +21,17 @@ template = """\
 (in-package :lem-user)
 
 (define-color-theme \"{name}\" ()
-  (foreground \"{fg}\")
-  (background \"{bg}\")
+  (foreground {fg})
+  (background {bg})
 {body})
 """
 
 def parse(style_string):
     for s in  style_string.split():
         if s.startswith("#"):
-            return "\"{}\"".format(s)
+            ix = s[1:] if len(s) == 7 else "".join(_ * 2 for _ in s[1:])
+            c = x256.to_rgb(x256.from_hex(ix))
+            return "\"#{:02x}{:02x}{:02x}\"".format(*c)
     return "nil"
 
 def translate(style_dict):
@@ -36,11 +39,11 @@ def translate(style_dict):
 
 def load_style(name):
     style =  pygments.styles.get_style_by_name(name)
-    bg = style.background_color
-    fg = style.styles[Text]
-    if not fg:
-        brightness = sum((int(bg[1:3],16),int(bg[3:5],16),int(bg[5:7],16))) / 3
-        fg = "#000000" if brightness >= 128 else "#ffffff"
+    bg = parse(style.background_color)
+    fg = parse(style.styles[Text])
+    if fg == "nil":
+        brightness =  sum(x256.to_rgb(x256.from_hex(bg[2:-1]))) / 3
+        fg = "\"#000000\"" if brightness >= 128 else "\"#FFFFFF\""
     style_dict = dict((str(k), parse(v)) for k, v in style.styles.items())
     return bg, fg, translate(style_dict)
 
@@ -48,7 +51,7 @@ def build_theme(name):
     bg, fg, attributes = load_style(name)
 
     body = "\n".join(
-            "  ({}-attribute :foreground {} :background \"{}\")".format(*_, bg)
+            "  ({}-attribute :foreground {} :background {})".format(*_, bg)
             for _ in attributes.items())
     with open("themes/{}.lisp".format(name), "w") as f:
         f.write(template.format(name=name, bg=bg, fg=fg, body=body))
